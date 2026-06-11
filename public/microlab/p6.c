9f843e9aef1ba2ea4ac0af1d1726f724a46e4f98
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-06-11 07:55:32 +0530

    Update p6.c

--- public/microlab/p6.c
+++ public/microlab/p6.c
@@ -1,22 +1,26 @@
+//C program using KEIL to sort the numbers in ascending/descending order using bubble sort.
 #include <lpc214x.h>
 
 int main(void)
 {
     volatile int i, j, temp;
-    int arr[] = {4,1,3,5,2};
+    int arr[] = {4, 1, 3, 5, 2};    // Array to be sorted
 
-    for(i = 1; i < 5; i++)
+    /* Bubble Sort */
+    for (i = 1; i < 5; i++)
     {
-        for(j = 0; j < 5 - i; j++)
+        for (j = 0; j < 5 - i; j++)
         {
-            if(arr[j] > arr[j + 1])
+            if (arr[j] > arr[j + 1])   // Compare adjacent elements
             {
-                temp = arr[j];
+                temp = arr[j];         // Swap if out of order
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
 
-    while(1);
+    while (1);     // Infinite loop
+
+    return 0;
 }
