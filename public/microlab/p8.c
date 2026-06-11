7e8a937daef948200ff5de159c76982c0583b508
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-06-11 07:57:16 +0530

    Update p8.c

--- public/microlab/p8.c
+++ public/microlab/p8.c
@@ -1,22 +1,28 @@
+//Case Convertor
 #include <lpc214x.h>
 
 int main(void)
 {
     volatile int i;
-    char tran_arr[20];
-    char arr[] = "MicroController";
 
-    for(i = 0; arr[i] != 0; i++)
+    char tran_arr[20];                 // Array to store translated string
+    char arr[] = "MicroController";    // Input string
+
+    for (i = 0; arr[i] != '\0'; i++)
     {
-        if(arr[i] >= 'a' && arr[i] <= 'z')
+        if (arr[i] >= 'a' && arr[i] <= 'z')
         {
-            tran_arr[i] = arr[i] - 32;
+            tran_arr[i] = arr[i] - 32; // Convert lowercase to uppercase
         }
-        else if(arr[i] >= 'A' && arr[i] <= 'Z')
+        else if (arr[i] >= 'A' && arr[i] <= 'Z')
         {
-            tran_arr[i] = arr[i] + 32;
+            tran_arr[i] = arr[i] + 32; // Convert uppercase to lowercase
         }
     }
 
-    while(1);
+    tran_arr[i] = '\0';                // String terminator
+
+    while (1);
+
+    return 0;
 }
